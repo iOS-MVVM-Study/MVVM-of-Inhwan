9f67968ea7d93d03f6c3ec22ee90e2f3b39e11b5
loinsir <a9327370@gmail.com>
2022-02-17 01:01:41 +0900

    탭바 뷰컨들에 네비게이션 설정

--- MainTabController.swift
+++ MainTabController.swift
@@ -14,15 +14,26 @@ class MainTabController: UITabBarController {
     
     private func setUI() {
         view.backgroundColor = .white
+        tabBar.barTintColor = .white
+        tabBar.isTranslucent = false
     }
     
     private func configureVCs() {
-        let feedVC = FeedVC()
-        let searchVC = SearchVC()
-        let imageSelectorVC = ImageSelectorVC()
-        let notificationsVC = NotificationVC()
-        let profileVC = ProfileVC()
+        let feedVC = templateNavigationVC(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedVC())
+        let searchVC = templateNavigationVC(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchVC())
+        let imageSelectorVC = templateNavigationVC(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorVC())
+        let notificationsVC = templateNavigationVC(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationVC())
+        let profileVC = templateNavigationVC(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileVC())
         
         viewControllers = [feedVC, searchVC, imageSelectorVC, notificationsVC, profileVC]
+    }
+    
+    private func templateNavigationVC(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        nav.navigationBar.isTranslucent = false
+        return nav
     }
 }
