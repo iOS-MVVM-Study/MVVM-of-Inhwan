48e014ee3c03cca9dca1154aebc76a4cf34cae68
loinsir <a9327370@gmail.com>
2022-03-02 17:47:20 +0900

    ProfileVC UI완성

--- MVVM-of-Inhwan/MVVM-of-Inhwan/View/Profile/ProfileCVC.swift
+++ MVVM-of-Inhwan/MVVM-of-Inhwan/View/Profile/ProfileCVC.swift
@@ -6,16 +6,30 @@ class ProfileCVC: UICollectionViewCell {
     
     static let identifier: String = "ProfileCVC"
     
+    private let postimageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        backgroundColor = .blue
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - func
+    
+    private func configureUI() {
+        addSubview(postimageView)
+        postimageView.fillSuperview()
     }
     
 }
